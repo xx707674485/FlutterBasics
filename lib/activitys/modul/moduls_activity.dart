2f752841e4a6eb42d4f39964db5a805f04bb7964
yangxichuan <yangxichuan2016@gmail.com>
2020-03-05 14:34:38 +0800

    Add iOS Certificate

--- lib/activitys/modul/moduls_activity.dart
+++ lib/activitys/modul/moduls_activity.dart
@@ -11,6 +11,9 @@ class _ModulsActivityState extends State<ModulsActivity> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Moduls"),
+      ),
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: Center(
         child: Text("Moduls Activity"),
